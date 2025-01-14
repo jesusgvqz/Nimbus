59e7df6a37dc42f07db5f2c0646161c9635b0297
Jesus Galvez Vazquez <111477236+jesusgvqz@users.noreply.github.com>
2025-01-13 21:19:13 -0600

    Delete venv/Nimbus directory

--- venv/Nimbus/Nimbus/decoradores.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect # type: ignore
-
-def login_requerido(vista):
-    def interna(request, *args, **kwargs):
-        if not request.session.get('logueado', False):
-            return redirect('/login')
-        return vista(request, *args, **kwargs)
-    return interna
--- venv/Nimbus/Nimbus_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NimbusAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Nimbus_app'
